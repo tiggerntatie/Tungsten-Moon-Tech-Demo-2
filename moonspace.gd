ad8c104c72f6064246ebb672299bb96f9fceb827
Eric Dennison <ericd@netdenizen.com>
2024-02-25 14:35:59 -0500

    Camera far-plane conflict resolved
    https://forum.godotengine.org/t/xrcamera3d-far-plane-limit/49164?u=tiggerntatie

--- moonspace.gd
+++ moonspace.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@onready var CAMERA : Camera3D = $"./Spacecraft/YawPivot/PitchPivot/Camera3D"
+@onready var XRCAMERA : XRCamera3D = $"./Spacecraft/YawPivot/PitchPivot/XROrigin3D/XRCamera3D"
 var xr_interface: XRInterface
 
 # Called when the node enters the scene tree for the first time.
@@ -14,9 +16,13 @@ func _ready():
 	
 		# Change our main viewport to putput to the HMD
 		get_viewport().use_xr = true
+		CAMERA.current = false
+		XRCAMERA.current = true
 	else:
 		print("OpenXR not initialized, please check if your headset is connected")
 		get_viewport().use_xr = false
+		CAMERA.current = true
+		XRCAMERA.current = false
 	# END VR Setup	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
