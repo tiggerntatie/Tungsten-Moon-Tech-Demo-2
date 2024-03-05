e10904c0996d1ebb318d5f36b9144de45ec7e36e
Eric Dennison <ericd@netdenizen.com>
2024-03-05 15:33:41 -0500

    Remove passing viewport size to sky shader

--- moonspace.gd
+++ moonspace.gd
@@ -7,25 +7,26 @@ var xr_interface: XRInterface
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# VR setup
-	xr_interface = XRServer.find_interface("OpenXR")
-	if xr_interface and xr_interface.is_initialized():
-		print("OpenXR initialised successfully")
+	var XRisup: bool = false
+	if XRServer.get_interface_count():
+		xr_interface = XRServer.find_interface("OpenXR")
+		if xr_interface and xr_interface.is_initialized():
+			XRisup = true
+			print("OpenXR initialised successfully")
+			
+			# Turn off v-sync
+			DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 		
-		# Turn off v-sync
-		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
-	
-		# Change our main viewport to putput to the HMD
-		get_viewport().use_xr = true
-		CAMERA.current = false
-		XRCAMERA.current = true
-	else:
-		print("OpenXR not initialized, please check if your headset is connected")
+			# Change our main viewport to putput to the HMD
+			get_viewport().use_xr = true
+			CAMERA.current = false
+			XRCAMERA.current = true
+	if not XRisup:
+		print("OpenXR not found or initialized; running in non-VR mode")
 		get_viewport().use_xr = false
 		CAMERA.current = true
 		XRCAMERA.current = false
 	# END VR Setup	
-	# Initialize sky shader
-	get_world_3d().environment.sky.sky_material.set("shader_parameter/viewport", get_viewport().size)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
