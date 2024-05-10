eb60bb1e358dd6534886c531463b3aa26165718a
Eric Dennison <ericd@netdenizen.com>
2024-05-10 16:01:21 -0400

    Spiffing up the basic button

--- spacecraft/controls/PanelButton/button.gd
+++ spacecraft/controls/PanelButton/button.gd
@@ -1,9 +1,10 @@
 @tool
 extends Node3D
 
-signal button_pressed(name: String, state: bool)
-signal button_released(name: String)
-signal button_set(name: String, state: bool)
+signal pressed(name: String, legend: String, state: bool, light_state: bool)
+signal released(name: String, legend: String, state: bool, light_state: bool)
+signal changed(name: String, legend: String, state: bool, light_state: bool)
+signal light_changed(name: String, legend: String, state: bool, light_state: bool)
 
 @export var legend : String = "X":
 	set(value):
@@ -22,10 +23,18 @@ signal button_set(name: String, state: bool)
 
 @export var is_toggle : bool = false
 
-## button light state
+@export var light_automatic : bool = true
+
+## button state
 @export var state : bool = false:
 	set(value):
 		state = value
+		_set_state()
+		
+## button light state
+@export var light_state : bool = false:
+	set(value):
+		light_state = value
 		_set_state()
 				
 func _update_button():
@@ -33,6 +42,14 @@ func _update_button():
 	$Button/MeshInstance3D.mesh.size.y = height
 	$Button/CollisionShape3D.shape.size.x = width
 	$Button/CollisionShape3D.shape.size.y = height
+	$Guard/Left.position.x = -width/2.0-$Guard/Left.mesh.size.x/2.0
+	$Guard/Left.mesh.size.y = height
+	$Guard/CollisionShapeLeft.position.x = -width/2.0-$Guard/CollisionShapeLeft.shape.size.x/2.0
+	$Guard/CollisionShapeLeft.shape.size.y = height
+	$Guard/Right.position.x = width/2.0+$Guard/Right.mesh.size.x/2.0
+	$Guard/Right.mesh.size.y = height
+	$Guard/CollisionShapeRight.position.x = width/2.0+$Guard/CollisionShapeRight.shape.size.x/2.0
+	$Guard/CollisionShapeRight.shape.size.y = height
 	$InteractableAreaButton/CollisionShape3D.shape.size.x = width
 	$InteractableAreaButton/CollisionShape3D.shape.size.y = height
 	$HandPoseArea/CollisionShape3D.shape.size.x = width + 0.005
@@ -41,7 +58,9 @@ func _update_button():
 	_set_state()
 	
 func _set_state():
-	$Button/MeshInstance3D.get_surface_override_material(0).emission_enabled = state
+	if light_automatic:
+		set_button_light(state)			
+	$Button/MeshInstance3D.get_surface_override_material(0).emission_enabled = light_state
 	if state:
 		$Button/Label3D.modulate = Color(0.1, 0.1, 0.1)
 	else:
@@ -55,16 +74,22 @@ func press_button() -> void:
 	if is_toggle:
 		set_button(not state)
 	else:
-		button_pressed.emit(legend, state)
+		pressed.emit(name, legend, state, light_state)
 	
 func release_button() -> void:
-	button_released.emit(legend)
+	released.emit(name, legend, state, light_state)
 
-# set the light state
+# set the state
 func set_button(value : bool) -> void:
-	state = value
-	_set_state()
-	button_set.emit(state)
+	if value != state:
+		state = value
+		changed.emit(name, legend, state, light_state)
+
+# set the state
+func set_button_light(value : bool) -> void:
+	if value != light_state:
+		light_state = value
+		light_changed.emit(name, legend, state, light_state)
 
 func _on_interactable_area_button_pressed(button):
 	press_button()
@@ -82,5 +107,6 @@ func _on_button_input_event(camera, event, position, normal, shape_idx):
 			release_button()
 
 func _on_mouse_exited():
-	$InteractableAreaButton._on_button_exited($".")
-	release_button()
+	if $InteractableAreaButton.pressed:
+		$InteractableAreaButton._on_button_exited($".")
+		release_button()
