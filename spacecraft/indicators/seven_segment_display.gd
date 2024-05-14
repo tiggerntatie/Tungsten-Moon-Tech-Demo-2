aec765115f87bfde129502cee73d5d0ffecc12fb
Eric Dennison <ericd@netdenizen.com>
2024-05-14 17:16:53 -0400

    Shader-based seven segment implementation

--- spacecraft/indicators/seven_segment_display.gd
+++ spacecraft/indicators/seven_segment_display.gd
@@ -1,53 +1,84 @@
 @tool
 extends Node3D
 
-@export var number_of_digits : int = 1:
+@export var display_value : float = 1.0:
+	set(value):
+		display_value = value
+		if Engine.is_editor_hint():
+			_layout_panel()
+			
+@export var display_is_valid : bool = true:
+	set(value):
+		display_is_valid = value
+		if Engine.is_editor_hint():
+			_layout_panel()
+		
+@export_range(1, 8, 1) var number_of_digits : int = 1:
 	set(value):
 		number_of_digits = value
 		if Engine.is_editor_hint():
-			_layout_digits()
-	
+			_layout_panel()
+			
+@export_range(0, 7, 1) var number_of_decimals : int = 0:
+	set(value):
+		number_of_decimals = value
+		if Engine.is_editor_hint():
+			_layout_panel()
+
+@export var display_colons : bool = false:
+	set(value):
+		display_colons = value
+		if Engine.is_editor_hint():
+			_layout_panel()
+
 @export var label_text : String = "":
 	set(value):
 		label_text = value
 		if Engine.is_editor_hint():
-			_layout_digits()
-			
-@export var digit_height : float = 1.5:
+			_layout_panel()
+
+@export var digit_height : float = 0.015:
 	set(value):
 		digit_height = value
 		if Engine.is_editor_hint():
-			_layout_digits()
+			_layout_panel()
+			
+@export var label_height : float = 0.01:
+	set(value):
+		label_height = value
+		if Engine.is_editor_hint():
+			_layout_panel()
 
-const FRAME_WIDTH = 0.1	# cm
-const DIGIT_WIDTH = 1.75
-const DIGIT_HEIGHT = 2.5
-var digit_array := []
-var digit_scene
-		
-func _layout_digits():
-	digit_scene = preload("res://spacecraft/indicators/seven_segment_digit.tscn")
-	# references to the tree
-	$PanelText.text = label_text
-	# clean out old children
-	for digit in digit_array:
-		digit.queue_free()
-	digit_array = []
-	var x_pos = 0.0
-	var y_pos = 0.0
-	var digit_index := 0
-	for digit_num in range(number_of_digits):
-		var inst = digit_scene.instantiate()
-		digit_array.push_back(inst)
-		add_child(inst)
-		inst.name = name + "Digit" + str(digit_num)
-		inst.position = Vector3(x_pos, y_pos, inst.position.z)
-		x_pos += DIGIT_WIDTH
-	$PanelText.position.x = x_pos + DIGIT_WIDTH*0.2
-	$PanelText.position.y = -DIGIT_HEIGHT/2.0
-	scale = Vector3.ONE * 0.01 * digit_height / DIGIT_HEIGHT
+const FRAME_WIDTH = 0.001	
+const FRAME_DEPTH = 0.003
+const DIGIT_ASPECT = 0.6
+
+func _layout_panel():
+	var digit_width = digit_height * DIGIT_ASPECT
+	var display_width = digit_width * number_of_digits
+	$Display.mesh.size = Vector2(display_width, digit_height)
+	$Display.position = Vector3(display_width/2.0, -digit_height/2.0, 0.001)
+	$Display.get_surface_override_material(0).set_shader_parameter("number_of_digits", number_of_digits)
+	$Display.get_surface_override_material(0).set_shader_parameter("value", display_value)
+	$Display.get_surface_override_material(0).set_shader_parameter("dashes", not display_is_valid)
+	$Display.get_surface_override_material(0).set_shader_parameter("decimals", number_of_decimals)
+	$Display.get_surface_override_material(0).set_shader_parameter("colons", display_colons)
+	$FrameL.mesh.size = Vector3(FRAME_WIDTH, digit_height, FRAME_DEPTH)
+	$FrameL.position = Vector3(-FRAME_WIDTH/2, -digit_height/2, FRAME_DEPTH/2)
+	$FrameR.mesh.size = Vector3(FRAME_WIDTH, digit_height, FRAME_DEPTH)
+	$FrameR.position = Vector3(display_width + FRAME_WIDTH/2, -digit_height/2, FRAME_DEPTH/2)
+	$FrameT.mesh.size = Vector3(display_width + FRAME_WIDTH*2, FRAME_WIDTH, FRAME_DEPTH)
+	$FrameT.position = Vector3(display_width/2, FRAME_WIDTH/2, FRAME_DEPTH/2)
+	$FrameB.mesh.size = Vector3(display_width + FRAME_WIDTH*2, FRAME_WIDTH, FRAME_DEPTH)
+	$FrameB.position = Vector3(display_width/2, -(digit_height+FRAME_WIDTH/2), FRAME_DEPTH/2)
 	
+	$PanelText.pixel_size = label_height / float($PanelText.font_size)
+	$PanelText.text = label_text
+	$PanelText.position = Vector3(display_width + digit_width*0.2, -digit_height/2.0, 0.001)
+
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_layout_digits()
+	_layout_panel()
