86032f9ada6c48dadcc3fff4231fee29ec605761
Eric Dennison <ericd@netdenizen.com>
2024-05-14 17:16:07 -0400

    Removed awful seven segment implementation

--- spacecraft/indicators/display_segment.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends Node3D
-
-@export var state : bool = false:
-	set(value):
-		state = value
-		if not Engine.is_editor_hint():
-			$MeshInstance3D.material_override.emission_enabled = state
--- spacecraft/indicators/display_segment_dp.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends MeshInstance3D
-
-@onready var state : bool = false:
-	set(value):
-		state = value
-		if not Engine.is_editor_hint():
-			material_override.emission_enabled = state
