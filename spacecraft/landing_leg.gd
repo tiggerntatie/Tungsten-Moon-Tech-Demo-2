9cd5f61dc8fd514832c87d7951ae52c563644a5a
Eric Dennison <ericd@netdenizen.com>
2024-05-23 12:49:59 -0400

    Programmatically generated landing legs
    0.0.13

--- spacecraft/landing_leg.gd
+++ spacecraft/landing_leg.gd
@@ -32,39 +32,90 @@ extends Node3D
 		if Engine.is_editor_hint():
 			_rebuild_model()
 	
-@export var angle_degrees : float = 45.0:
+@export var bar_diameter : float = 0.15:
 	set(value):
-		angle_degrees = value
+		bar_diameter = value
+		if Engine.is_editor_hint():
+			_rebuild_model()
+
+@export var pad_diameter : float = 0.8:
+	set(value):
+		pad_diameter = value
+		if Engine.is_editor_hint():
+			_rebuild_model()
+
+@export var pad_height : float = 0.4:
+	set(value):
+		pad_height = value
+		if Engine.is_editor_hint():
+			_rebuild_model()
+
+@export var leg_material : StandardMaterial3D:
+	set(value):
+		leg_material = value
 		if Engine.is_editor_hint():
 			_rebuild_model()
 
 
+func _build_leg(angle : float, r : float, span : float):
+	var top_bar := MeshInstance3D.new()
+	add_child(top_bar)
+	top_bar.mesh = CapsuleMesh.new()
+	top_bar.mesh.radius = bar_diameter / 2.0
+	top_bar.mesh.radial_segments = 16
+	top_bar.rotation.z = atan(span/(ship_bottom + vertical_sep))
+	top_bar.mesh.height = sqrt((ship_bottom+vertical_sep)**2.0 + span**2.0)
+	top_bar.rotation.y = angle
+	top_bar.position.x = (r + span/2.0)*cos(angle)
+	top_bar.position.z = -(r + span/2.0)*sin(angle)
+	top_bar.position.y = (ship_bottom + vertical_sep)/2.0
+	top_bar.set_surface_override_material(0, leg_material)
+	var left_bar := MeshInstance3D.new()
+	add_child(left_bar)
+	left_bar.mesh = CapsuleMesh.new()
+	left_bar.mesh.radius = bar_diameter / 2.0
+	left_bar.mesh.radial_segments = 16
+	left_bar.rotation.z = atan(span/(ship_bottom + 0.2))
+	left_bar.mesh.height = sqrt((ship_bottom+0.2)**2.0 + span**2.0 + 0.5**2.0)
+	left_bar.rotation.y = angle - atan(0.5 / span)
+	left_bar.position.x = (r + span/2.0)*cos(angle) - 0.25*sin(angle)
+	left_bar.position.z = -(r + span/2.0)*sin(angle) - 0.25*cos(angle)
+	left_bar.position.y = (ship_bottom + 0.2)/2.0
+	left_bar.set_surface_override_material(0, leg_material)
+	var right_bar := MeshInstance3D.new()
+	add_child(right_bar)
+	right_bar.mesh = CapsuleMesh.new()
+	right_bar.mesh.radius = bar_diameter / 2.0
+	right_bar.mesh.radial_segments = 16
+	right_bar.rotation.z = atan(span/(ship_bottom + 0.2))
+	right_bar.mesh.height = sqrt((ship_bottom+0.2)**2.0 + span**2.0 + 0.5**2.0)
+	right_bar.rotation.y = angle + atan(0.5 / span)
+	right_bar.position.x = (r + span/2.0)*cos(angle) + 0.25*sin(angle)
+	right_bar.position.z = -(r + span/2.0)*sin(angle) + 0.25*cos(angle)
+	right_bar.position.y = (ship_bottom + 0.2)/2.0
+	right_bar.set_surface_override_material(0, leg_material)
+	var pad := MeshInstance3D.new()
+	add_child(pad)
+	pad.mesh = CylinderMesh.new()
+	pad.mesh.bottom_radius = pad_diameter / 2.0
+	pad.mesh.top_radius = pad_diameter / 2.0
+	pad.mesh.height = pad_height
+	pad.mesh.radial_segments = 16
+	pad.position.y = pad_height/2.0
+	pad.position.x = sqrt(2.0)*pad_span/2.0*cos(angle)
+	pad.position.z = -sqrt(2.0)*pad_span/2.0*sin(angle)
+	pad.set_surface_override_material(0, leg_material)
+
 
 func _rebuild_model():
-	var angle = deg_to_rad(angle_degrees)
+	for child in get_children():
+		child.queue_free()
 	var r = sqrt(2.0)*(ship_width + ship_front_width)/4.0
 	var span = sqrt(2.0)*pad_span/2.0 - r
-	$TopBar.rotation.z = atan(span/(ship_bottom + vertical_sep))
-	$TopBar.mesh.height = sqrt((ship_bottom+vertical_sep)**2.0 + span**2.0)
-	$TopBar.rotation.y = angle
-	$TopBar.position.x = (r + span/2.0)*cos(angle)
-	$TopBar.position.z = -(r + span/2.0)*sin(angle)
-	$TopBar.position.y = (ship_bottom + vertical_sep)/2.0
-	$LeftBar.rotation.z = atan(span/(ship_bottom + 0.2))
-	$LeftBar.mesh.height = sqrt((ship_bottom+0.2)**2.0 + span**2.0 + 0.5**2.0)
-	$LeftBar.rotation.y = angle - atan(0.5 / span)
-	$LeftBar.position.x = (r + span/2.0)*cos(angle) - 0.25*sin(angle)
-	$LeftBar.position.z = -(r + span/2.0)*sin(angle) - 0.25*cos(angle)
-	$LeftBar.position.y = (ship_bottom + 0.2)/2.0
-	$RightBar.rotation.z = atan(span/(ship_bottom + 0.2))
-	$RightBar.mesh.height = sqrt((ship_bottom+0.2)**2.0 + span**2.0 + 0.5**2.0)
-	$RightBar.rotation.y = angle + atan(0.5 / span)
-	$RightBar.position.x = (r + span/2.0)*cos(angle) + 0.25*sin(angle)
-	$RightBar.position.z = -(r + span/2.0)*sin(angle) + 0.25*cos(angle)
-	$RightBar.position.y = (ship_bottom + 0.2)/2.0
-	$Pad.position.y = $Pad.mesh.height/2.0
-	$Pad.position.x = sqrt(2.0)*pad_span/2.0*cos(angle)
-	$Pad.position.z = -sqrt(2.0)*pad_span/2.0*sin(angle)
+	_build_leg(PI/4.0, r, span)
+	_build_leg(3.0*PI/4.0, r, span)
+	_build_leg(5.0*PI/4.0, r, span)
+	_build_leg(7.0*PI/4.0, r, span)
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
