1a2305588f9316a26576958d5b0b4314b6d93555
Eric Dennison <ericd@netdenizen.com>
2024-06-02 16:35:59 -0400

    Adjust range depending on the moon scale

--- spacecraft/landing_light.gd
+++ spacecraft/landing_light.gd
@@ -1,9 +1,14 @@
 extends SpotLight3D
 
+@onready var default_landing_light_range : float = spot_range
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Signals.connect("ButtonLight_changed", _on_landing_light_button_changed)
+	Signals.connect("moon_scale_changed", _on_moon_scale_changed)
 
 func _on_landing_light_button_changed(state, lightstate):
 	visible = state
+
+func _on_moon_scale_changed(scale_factor: float) -> void:
+	spot_range = default_landing_light_range / scale_factor
