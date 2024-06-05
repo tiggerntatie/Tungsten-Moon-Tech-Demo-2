a9b4cd2e74835cc13e6e061b01401bf2391d68a4
Eric Dennison <ericd@netdenizen.com>
2024-06-05 15:15:14 -0400

    Add a global signal for notifying when a terrain object has been relocated

--- signals.gd
+++ signals.gd
@@ -1,3 +1,6 @@
 extends Node
 
-# Signals will be added to this node programmatically
+# global signals that must be defined first
+signal terrain_object_moved(identifier : String, lon: float, lat: float)
+
+# Signals may be added to this node programmatically
