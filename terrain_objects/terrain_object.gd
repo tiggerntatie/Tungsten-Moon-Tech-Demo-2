c578d0fa955c64d40b53a1d4884f51627f9da86a
Eric Dennison <ericd@netdenizen.com>
2024-06-05 15:16:13 -0400

    Base terrain object script

--- terrain_objects/terrain_object.gd
+++ terrain_objects/terrain_object.gd
@@ -4,11 +4,20 @@ class_name TerrainObject
 
 @export var moon_data : MoonData
 
+## Identifier of the object
+@export var identifier : String
+
 ## Longitude of the object in degrees
-@export var longitude : float
+@export var longitude : float:
+	set(value):
+		longitude = value
+		Signals.emit_signal("terrain_object_moved", identifier, longitude, latitude)
 
 ## Latitude of the object in degrees
-@export var latitude : float
+@export var latitude : float:
+	set(value):
+		latitude = value
+		Signals.emit_signal("terrain_object_moved", identifier, longitude, latitude)
 
 ## Heading of the object in degrees
 @export var heading : float
@@ -16,11 +25,22 @@ class_name TerrainObject
 ## Altitude offset (increase/decrease height)
 @export var altitude_adjust : float = 0.0
 
+@export_category("Relative position")
+## Compute relative position from reference object
+@export var relative_position : bool = false
+
+## Reference terrain object
+@export var reference_object : TerrainObject
+
+## Departure bearing from reference object, in degrees
+@export var departure_bearing : float = 0.0
+
+## Distance from reference object, in meters
+@export var distance_from_reference : float = 100.0
+
 # range at which the object becomes invisible
 const VISIBLE_RANGE := 10000.0
-const BURIED_LENGTH := 10.0	# nominal length of pedestal buried beneath ground
-@onready var PAD : MeshInstance3D = $StaticBody3D/PadSurface
-@onready var PEDESTAL : MeshInstance3D = $StaticBody3D/Pedestal
+
 
 # default moon scale 
 var default_moon_scale_set : bool = false
@@ -68,6 +88,16 @@ func _ready():
 		Signals.add_user_signal("Spacecraft_reset")
 	Signals.connect("moon_position_changed", _on_moon_position_changed)
 	Signals.connect("Spacecraft_reset", _on_spacecraft_reset)
+	# compute a position relative to another terrain object
+	if relative_position:
+		var position_array = moon_data.get_destination_from_bearing_and_distance(
+			reference_object.latitude,
+			reference_object.longitude,
+			departure_bearing,
+			distance_from_reference
+		)
+		longitude = position_array[1]
+		latitude = position_array[0]
 	rotation = moon_data.get_object_surface_rotation(latitude, longitude, heading)
 	dv_relative_position = DVector3.FromVector3(moon_data.get_vector_position(latitude, longitude))
 	dv_relative_position.multiply_scalar(
@@ -75,11 +105,6 @@ func _ready():
 		moon_data.radius
 	)
 	position = dv_relative_position.vector3()
-	$StaticBody3D.position.y = altitude_adjust
-	PEDESTAL.mesh.top_radius = PAD.mesh.top_radius/4
-	PEDESTAL.mesh.bottom_radius = PEDESTAL.mesh.top_radius
-	PEDESTAL.mesh.height = altitude_adjust + BURIED_LENGTH
-	PEDESTAL.position = Vector3(0.0, -PEDESTAL.mesh.height/2.0, 0.0)
 	top_level = true
 
 # periodically update the object position and make visible if necessary
